6a8b065e9f57564bc8b55ab21192761cba24b492
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-01-05 01:34:41 +0530

    Update common_el_3_arrays.js

--- Coding_Questions/common_el_3_arrays.js
+++ Coding_Questions/common_el_3_arrays.js
@@ -1,1 +1,25 @@
+// WAP to find out commmon elements from three arrays.
 
+var a = [1,2,3,4,5];
+var b = [3,4,5,6,7];
+var c = [4,5,6,7,8];
+
+var i = 0, j = 0, k = 0;
+
+while(i < a.length && j < b.length && k < c.length){
+    if(a[i] == b[j] && b[j] == c[k]){
+        console.log(a[i]);
+        i++;
+        j++;
+        k++;
+    }else if(a[i] < b[j]){
+        i++;
+    }else if(b[j] < c[k]){
+        j++;
+    }else{
+        k++;
+    }
+}
+
+
+// o/p -->> 4 5 
