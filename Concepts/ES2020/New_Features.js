aaf51e533a038472fe1f7d92cab79391bed70bc6
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-01-05 02:05:58 +0530

    Update New_Features.js

--- Concepts/ES2020/New_Features.js
+++ Concepts/ES2020/New_Features.js
@@ -1,7 +1,13 @@
 //Javascript has introduced ES2020 with 5 cool new features.
 
 //1. GlobalThis
+// why we need GlobalThis??
 
+// in JS window, this, self, frames all these refer to window object.
+// but if you are using web worker then only "self" will work there.
+// in node.js there is seprate object called global. so to make it common on all platforms JS has introduced globalThis object.
+
+conosle.log(globalThis);    //Window {parent: Window, opener: null, top: Window, length: 1, frames: Window, …}
 
 //2. Promise.allSettled()
 
