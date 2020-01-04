e714fa220c0e09725f47dd6f69b322ca506f94f4
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-01-05 01:57:16 +0530

    Update New_Features.js

--- Concepts/ES2020/New_Features.js
+++ Concepts/ES2020/New_Features.js
@@ -1,1 +1,15 @@
+//Javascript has introduced ES2020 with 5 cool new features.
 
+//1. GlobalThis
+
+
+//2. Promise.allSettled()
+
+
+//3. Nullish CoalSecing Operator
+
+
+//4. Optional chaning operator 
+
+
+//5. BigInt
