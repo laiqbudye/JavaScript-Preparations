4f4927d8c59bf21ce5add0a7cf825c3563b2e9d9
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-01-22 23:31:19 +0530

    Update Destructuring.js

--- Concepts/ES6/Destructuring.js
+++ Concepts/ES6/Destructuring.js
@@ -1,5 +1,5 @@
 
-//What is Destructuring?
+//1. What is Destructuring?
 
 //Destructuring is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, 
 //into distinct variables. That is, we can extract data from arrays and objects and assign them to variables.
@@ -21,3 +21,11 @@ let [a, ,b] = numArray;       // a: 10 b: 30
 
 // If we add more variable than array length then?
 let [a,b,c,d] = numArray; // a: 10 b: 20 c: 30 d: undefined 
+
+
+//2. Rest Operator
+//We can copy array elements using rest operator also as below:-
+
+let [...b] = numArray;  // b: 10,20,30
+
+let [a,...b] = numArray;   //a:10, b:20,30
