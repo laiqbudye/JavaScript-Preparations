2d34e2c3d7ff47b41a96bbfaf9e0a0d79fee7bd0
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-01-23 00:17:53 +0530

    Update Destructuring.js

--- Concepts/ES6/Destructuring.js
+++ Concepts/ES6/Destructuring.js
@@ -36,14 +36,32 @@ let [a,...b] = numArray;   //a:10, b:20,30
 // Uses of Destructuring in objects
 
 let personObject = { name: 'Laiq', lastname: 'Budye', address: 'Pune' };
-
 let {name, lastname} = personObject;    // you have to give property name in let{}.  here {} is not define object it's desturcturing syntax
 
 console.log('Name: ' + name + ' LastName: ' + lastname);    //Name: Laiq  Lastname: Budye
 
 // below is the tricky thing that you can not skip and expect to work it like array
-
 let {name, , lastname} = personObject;  // this will throw an error here, Syntax error: Unexpected token
 
 
+     
+// Destructuring in nested deep level data
+     
+const wes = {
+  first: 'Wes',
+  last: 'Bos',
+  links: {
+    social: {
+      twitter: 'https://twitter.com/wesbos',
+      facebook: 'https://facebook.com/wesbos.developer',
+    },
+    web: {
+      blog: 'https://wesbos.com'
+    }
+  }
+};
+   
+const { twitter, facebook } = wes.links.social;
+console.log(twitter, facebook); // logs the 2 variables 
+     
 //for more info:- https://wesbos.com/destructuring-objects/
