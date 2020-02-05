65bc876f3412860bc97052e8c7b144cab80d2fd3
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-02-05 16:45:43 +0530

    Update 5.Arrow_Functions.js

--- Concepts/JS/5.Arrow_Functions.js
+++ Concepts/JS/5.Arrow_Functions.js
@@ -47,3 +47,14 @@ let welcome = (age < 18) ?
   () => alert("Greetings!");
 
 welcome(); // Greetings!
+
+
+
+//for Multi line Arrow Functions
+
+let sum = (a, b) => {  // the curly brace opens a multiline function
+  let result = a + b;
+  return result; // if we use curly braces, then we need an explicit "return"
+};
+
+alert( sum(1, 2) ); // 3
