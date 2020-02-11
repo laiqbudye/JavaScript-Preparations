93029ca32cb1fbdd1da1c9719f55f61f90160b15
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-02-11 23:53:22 +0530

    Update 10.call_apply_bind.js

--- Concepts/JS/10.call_apply_bind.js
+++ Concepts/JS/10.call_apply_bind.js
@@ -1,1 +1,46 @@
 
+// Every function has access to following properties:-
+// 1.Name - function name, this is optional, function can be anonymous also
+// 2.Code - this is the block which gets execute when function is invoked.
+// 3.call()
+// 4.apply()
+// 5.bind()
+
+// 1. Call & Apply
+// these method can be used for function borrowing, which will avoid code repitition.
+// when we call any function JS internally calls call method on  that function.
+
+// e.g
+
+function eat(){
+console.log("I am eating");
+}
+
+eat();      // JS internally call it as eat.call();
+
+// call method calls the function with given this scope and arguments. we can specify explicitly that to which object this should refer.
+
+// The main differences between bind() and call() is that the call() method:
+// 1. Accepts additional parameters as well
+// 2. Executes the function it was called upon right away.
+// 3. The call() method does not make a copy of the function it is being called on.
+
+
+// call() and apply() serve the exact same purpose. The only difference between how they work is that call() expects all parameters 
+// to be passed in individually, whereas apply() expects an array of all of our parameters. Example:
+
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);    // here we borrowed function from pokemon object.
+};
+
+pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
+pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
