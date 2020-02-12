e0a1ec429ed1a17fbbb70c93f35ee657c60a0491
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-02-12 19:51:31 +0530

    Update 11.Functions_Are_Objects.js

--- Concepts/JS/11.Functions_Are_Objects.js
+++ Concepts/JS/11.Functions_Are_Objects.js
@@ -26,3 +26,38 @@ lastname: 'Budye',         // refers to the property that we have added explicit
 name: 'print',            // refers to the function name of the function
 () : console.log("Laiq");   // refers to code of the function
 }
+
+
+// Functions are FIRST CLASS CITIZENS in js because
+
+// 1. we can assign functions to the varible
+
+var a = function(){
+console.log("I am function");
+}
+
+// 2. we can pass one function to another as a parameter
+
+function a(){
+console.log("I am A");
+}
+
+function b(fn){
+  fn();
+}
+
+b(a);      // I am A
+
+//3. we can return one function form other function
+
+function x(){
+  return function y(){
+  console.log("I am Y");
+  }  
+}
+
+x();      //return function y()
+x()();      //I am Y
+
+
+//because of all this functions are FIRST CLASS CITIZENS in JS.
