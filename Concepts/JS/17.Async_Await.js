3fcb339c0654568d7370462cbc6d491662e2f443
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-02-21 00:24:51 +0530

    Update 17.Async_Await.js

--- Concepts/JS/17.Async_Await.js
+++ Concepts/JS/17.Async_Await.js
@@ -1,1 +1,22 @@
 
+// Async / Await
+async function init() {   //we need to write async keyword in front of function
+  await createPost({ title: 'Post Three', body: 'This is post three' });    // awiat will wait for the statement to get execute completely.
+
+  getPosts();
+}
+
+init();
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
+  // Asunc / Await / Fetch
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+  const data = await res.json();
+
+  console.log(data);
+}
+
+fetchUsers();
