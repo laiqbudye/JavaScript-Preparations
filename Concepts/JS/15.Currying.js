57ef44ff61e5f5dc280119c7f40cc808c400dfb4
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-02-20 19:45:30 +0530

    Create 15.Currying.js

---
+++ Concepts/JS/15.Currying.js
@@ -0,0 +1,4 @@
+//A curried function is a function that takes multiple arguments one at a time. 
+//Given a function with 3 parameters, the curried version will take one argument and return a function that takes the next argument, 
+//which returns a function that takes the third argument. The last function returns the result of applying the function to all of its 
+//arguments.
