d101e8ac41fe31181cb7b08b4ce480c4b440d1a0
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-02-21 00:18:04 +0530

    Update 16.Promise.js

--- Concepts/JS/16.Promise.js
+++ Concepts/JS/16.Promise.js
@@ -1,1 +1,66 @@
 
+// Promise introduced in ES6. before promised we were using callback funtions.
+
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' }
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;   //printing post's title
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();         //getposts() will get execute here 
+  }, 2000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);    // here we passed getPosts as callback function 
+
+
+*********************************************************************************************************************************
+  
+//   to overcome this issue promises comes into picture.
+  
+  const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' }
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject('Error: Something went wrong');
+      }
+    }, 2000);
+  });
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' })
+  .then(getPosts)       // if it gets resolved then this line will get execute
+  .catch(err => console.log(err));    // if some error occurs this will catch that errror
