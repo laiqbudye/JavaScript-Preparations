c50e1e410ecc8d58c285bc3045e5e50ff8295100
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-08-18 00:52:52 +0530

    Update Arrow_Functions.js

--- Concepts/ES6/Arrow_Functions.js
+++ Concepts/ES6/Arrow_Functions.js
@@ -24,3 +24,58 @@ const printName = (name) => {
 }
 
 printName('Laiq');
+
+-----------------------------------------------------------
+  
+  
+var obj = {
+    name1: 'Iqbal',
+    print: function(){
+        setTimeout(function(){
+            console.log(this.name1);   // undefined, as this is poining to window heree
+        },0)
+    }
+}
+
+obj.print();
+
+// this is problem with normal function as it is dynamically scoped ie. this obj inside print method points to obj,,, but inside setTimeout func it is pointing to window obj.
+
+//we can solve this issue by using arrow function
+var obj = {
+    name1: 'Iqbal',
+    print: function(){
+        console.log(this);
+        setTimeout(() => {
+            console.log(this.name1);   // Iqbal
+        },0)
+    }
+}
+// here THIS is lexically scoped, means line 50 will get lexical this passed by settimeout func,,, & settimeout gets lexical this passed by print func which is pointing to obj.
+
+// we can also solve this issue by using bind method
+
+var obj = {
+    name1: 'Iqbal',
+    print: function(){
+        console.log(this);
+        setTimeout(function() {
+            console.log(this.name1);   // Iqbal
+        }.bind(this),0)
+    }
+}
+
+
+// also there is most widely used approach to solve this as below
+
+var obj = {
+    name1: 'Iqbal',
+    print: function(){
+        var that = this;
+        setTimeout(function() {
+            console.log(that.name1);  // Iqbal
+        }.bind(this),0)
+    }
+}
+
+
