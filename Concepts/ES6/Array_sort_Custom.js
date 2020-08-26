a921cee908ee797c9fc8ff0825593db44c56814b
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-08-27 00:38:42 +0530

    Create Array_sort_Custom.js

---
+++ Concepts/ES6/Array_sort_Custom.js
@@ -0,0 +1,42 @@
+// Array.sort()
+
+//sort method sorts the number, strings, objects.
+
+//e.g
+const arr = [5,8,1,4,9,7]
+
+arr.sort()
+
+console.log(arr);  //  [1, 4, 5, 7, 8, 9]
+
+--------------------------------------------------------------------------
+
+const arr = [85,18,21,244,9,777]
+
+arr.sort()
+
+console.log(arr);  // [18, 21, 244, 777, 85, 9]
+
+//in the above example we can see some weird o/p. this is because of sort method does sorting on the basis of first letter... (first it convert number to string, and then do sorting
+on the basis of first letter of the string)
+
+// we can solve this problem by passing custom function to sort method.
+
+const arr = [85,18,21,244,9,777]
+arr.sort(compare)   // [9, 18, 21, 85, 244, 777]
+console.log(arr);
+
+function compare(a,b){
+    return a-b;    // this is for ascending result
+    return b-a;    // this is for descending result
+}
+
+
+//ES6 version of same example
+
+const arr = [85,18,21,244,9,777]
+arr.sort((a,b) => a-b)   // [9, 18, 21, 85, 244, 777]
+console.log(arr);
+
+-------------------------------------------------------------------------------------------------------------------
+
