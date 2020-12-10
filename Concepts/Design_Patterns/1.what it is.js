98fe00bcc9d3fc1edb375652ba40b8e95b6800a7
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2020-12-10 19:02:43 +0530

    Update 1.what it is.js

--- Concepts/Design_Patterns/1.what it is.js
+++ Concepts/Design_Patterns/1.what it is.js
@@ -1,3 +1,9 @@
 
 //1.What is Design Pattern?
 // Design patterns are common architechtural approaches that has been observed in software engg to solve some issues.
+
+//There are 3 types of design patterns.
+
+1. Creational
+2. Structural
+3. Behavioural
