119dd80b47bfb7e80a212c113d22c3c69065f527
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-01-06 23:23:56 +0530

    Update 19. Comparing_objects.js

--- Concepts/JS/19. Comparing_objects.js
+++ Concepts/JS/19. Comparing_objects.js
@@ -57,7 +57,81 @@ here it will chek for strict equality of state.favNumber === state2.favNumber  &
  
  so the above two objects are not shallow equal in this case.
  
+
+for more reference:- https://scrimba.com/scrim/c2Paa6uV
  
  
  
  2. Deep Comparison
+
+ in the deep comparison it checks for strict equality of all the properties including nested objects.
+ 
+ 
+ const state = {
+    favNumber: 42,
+    name: "Bob",
+    address: {
+        street: "123 Main Street",
+        city: "Nowhere, PA",
+        zip: 12345
+    }
+}
+
+
+const state2 = {
+    favNumber: 42,
+    name: "Bob",
+    address: {
+        street: "123 Main Street",
+        city: "Nowhere, PA",
+        zip: 12345
+    }
+}
+
+
+in the above example it will chek for each & every property including nested objects/arrays for strict equality.
+
+
+examples are as follows:-
+    
+    user1 = {
+    name: "John",
+    address: {
+        line1: "55 Green Park Road",
+        line2: "Purple Valley"  
+    }
+}
+
+user2 = user1;
+// user1 === user2 true
+// shallowEqual(user1, user2) true
+// deepEqual(user1, user2) true
+
+
+
+user2 = {
+    name: "John",
+    address: user1.address
+}
+// user1 === user2 false
+// shallowEqual(user1, user2) true
+// deepEqual(user1, user2) true
+
+
+
+ user2 = {
+    name: "John",
+    address: {
+        line1: "55 Green Park Road",
+        line2: "Purple Valley" 
+    }
+}
+// user1 === user2 false
+// shallowEqual(user1, user2) false
+// deepEqual(user1, user2) true
+
+for more reference:- https://www.mattzeunert.com/2016/01/28/javascript-deep-equal.html
+ 
+ 
+ 
+ 
