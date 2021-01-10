f63c34b1a4449a322c51c307b76341d2290c8e5c
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-01-11 02:09:36 +0530

    Update 1. Questions_asked_to_me.js

--- Interview_Questions/1. Questions_asked_to_me.js
+++ Interview_Questions/1. Questions_asked_to_me.js
@@ -3,7 +3,7 @@
 1. How many threads in JS. How does it handles async tasks. 
 2. What is event loop (microtasks macrotasks) 
 3. Call apply bind difference
-4. Closure example of settimeout & how to solve that
+4. Closure example of settimeout & how to solve that, write code
 5. How many Api requests browsers can handle at a time
 6. React life cycle of all phases with sequence
 7. Hooks euiavlent to life cycle methods
@@ -12,3 +12,5 @@
 10. getSnapshotBeforeUpdate usage with example
 11. Redux connect method usage
 12. Error boundary
+13. ShouldComponentUpdate performs shallow or deep comparison
+14. Shallow vs deep comparison
