d1675351a2032473cf7ecf0b56dc1339fe77d4b2
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-01-19 19:24:56 +0530

    Create 20. pollyfills.js

---
+++ Concepts/JS/20. pollyfills.js
@@ -0,0 +1,52 @@
+Pollyfill is a piece of code that usually use to provide modern functionality on older browsers that natively do not support it.
+
+
+1. pollyfill for Array.map() method
+
+Array.prototype.myMap = function(callback){
+    let newArray = [];
+    for(let i = 0; i<this.length; i++){
+        let result = callback(this[i], i);
+
+        newArray.push(result);
+    }
+    return newArray;
+}
+
+---------------------------------------------------------------------------------------------------
+
+2. pollyfill for Array.filter() method
+
+Array.prototype.myFilter = function(callback){
+    let newArray = [];
+    for(let i = 0; i<this.length; i++){
+        let result = callback(this[i], i);
+        
+        if(result){
+            newArray.push(this[i]);
+        }
+        
+    }
+    return newArray;
+}
+
+---------------------------------------------------------------------------------------------------
+
+3. pollyfill for bind() method
+
+Function.prototype.mybind = function(context, args1){
+    let fn = this;
+    return function (args2){
+        return fn.apply(context, [args1, args2])
+    }
+}
+
+function add(a,b) {
+    return a+b
+}
+
+var mybinded = add.mybind(this,2)
+
+var result = mybinded(18);
+
+console.log(result);
