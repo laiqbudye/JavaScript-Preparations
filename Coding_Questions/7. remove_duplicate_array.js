e5e50552e031593b24a2587d1059c209e5a96d8b
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-02-12 01:21:33 +0530

    Update 7. remove_duplicate_array.js

--- Coding_Questions/7. remove_duplicate_array.js
+++ Coding_Questions/7. remove_duplicate_array.js
@@ -5,8 +5,36 @@
 
 var arr = [1,2,3,4,3,1,2];
 
-function removeDuplicates(arr){
-  arr.filter((item, index) => arr.indexOf(item) === index);
+function removeDuplicates(arr) {
+  return arr.filter((item, index) => arr.indexOf(item) === index);
 }
 
 var uniqArr = removeDuplicates(arr);
+
+
+
+//2. Es6 approach with set
+
+var arr = [1,2,3,4,3,1,2];
+
+var uniqArr = [...new Set(arr)];
+
+
+
+//3. using for loop
+
+var arr = [1,2,3,4,3,1,2];
+
+function unique(data){
+let uniqarr = [];
+  
+  for (let i=0; i < data.length; i++){
+    if(uniqarr.indexOf(data[i]) < 0){
+      uniqarr.push(data[i]);
+    }
+  }
+  return uniqarr;
+}
+
+
+unique(arr)
