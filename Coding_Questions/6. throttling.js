453f18985455ee99245e1af4f6235f5d9e3468b0
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-02-14 17:33:37 +0530

    Update 6. throttling.js

--- Coding_Questions/6. throttling.js
+++ Coding_Questions/6. throttling.js
@@ -1,1 +1,24 @@
 
+function throttle(fn, delay){
+    let timer;
+
+    return function(){
+        let context = this;
+        let args = arguments;
+
+        if(!timer){
+            fn.apply(context, args);
+            timer = true;
+
+            setTimeout(function(){
+                timer = false;
+            }, delay)
+        }
+    }
+}
+
+function print(){
+    console.log("Printing");
+}
+
+var a = throttle(print, 2000);
