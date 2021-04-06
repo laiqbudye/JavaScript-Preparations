b33085b2f5044dac41150cdd75790357caad53f1
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-04-06 13:45:14 +0530

    Delete 1.Applied_for.js

--- Concepts/My_Interview_Process/1.Applied_for.js
+++
@@ -1,7 +0,0 @@
-1. worc.co.in - LinkedIn Post - responded saying exp is less - she will share resume with technical panel & get back.
-
-2. iProgrammer  - LinkedIn Post -  responded with google form link that i need to fill
-
-3. Accenture - Naukri - no response till now
-
-4. satincorp - LinkedIn Chat - responded with email id on which i need to forword my resume
