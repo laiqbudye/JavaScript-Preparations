545dd9c17aa5f0da463d38bddd8a266ce4611da7
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-04-10 02:26:08 +0530

    Update 12. numberToDigits.js

--- Coding_Questions/12. numberToDigits.js
+++ Coding_Questions/12. numberToDigits.js
@@ -17,3 +17,19 @@ function numberToDigits(numArray) {
 }
 
 numberToDigits([12,34,56]);   // [1,2] [3,4] [5,6]
+
+
+
+
+--------------------------------same example with inBuilt functions ----------------------------
+
+function numberToDigits(numArray) {
+    let result = [];
+    for(let i = 0; i< numArray.length; i++){
+        let digits = numArray[i].toString().split('');   // converting number to string & splitting the number
+        result = result.concat(digits);
+    }
+    return result.map(number => +number);
+}
+
+numberToDigits([12,34,232]);  // [1, 2, 3, 4, 2, 3, 2]
