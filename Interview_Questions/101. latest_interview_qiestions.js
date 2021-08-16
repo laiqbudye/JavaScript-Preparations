831b9fe9990f3c916510274868b387e9ca5c2d29
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-16 20:05:14 +0530

    Update 101. latest_interview_qiestions.js

--- Interview_Questions/101. latest_interview_qiestions.js
+++ Interview_Questions/101. latest_interview_qiestions.js
@@ -1,1 +1,99 @@
 
+HTML
+1. what are Sematic tags? how they are different from normal tags
+2. localstorage vs sessionstorage
+3. ways to connect JS file with HTML
+4. Async vs Defer
+5. what is DOCTYPE?
+6. mention new tags in HTML5
+7. in Html file, if footer is placed on top & header is at bottom then how it will render on webpage.
+
+
+CSS
+1. inline-block vs inline vs block
+2. css specificity
+3. different positions, which is default one
+4. different ways to center a div
+5. how CSS is different than SASS? what is need of SASS if we already have CSS
+6. box-sizing property
+7. relative vs absolute units
+8. responsive design (media queries)
+
+
+JAVASCRIPT
+1. data types
+2. coding questions on pass by reference (objects/arrays)
+3. coding questions on hoisting
+4. var let const difference? what happens if we declare them in same scope
+5. prototypes
+6. how prototype works in classes
+7. use strict
+8. event bubbling/capturing
+9. code to find out each letter's count in a string & return in object
+e.g AMAZON => {A:2, M:1, Z:1, O:1, N:1}
+10. closure vala code (setTimeout example) & solve that problem
+11. why arrow functions got introduce? what problem they solve
+12. how to copy array/objects? include deep copy as well as shallow copy
+13. how to sort array of objects? explain with example.
+14. Array.map() vs Array.filter(), Array.slice() vs Array.splice()
+15. How many threads are there in JS. How does it handles async tasks. 
+16. What is event loop (microtasks, macrotasks) 
+17. Call apply bind difference
+18. how to flatten an array without using any third party library
+19. thrttling vs Debouncing (may ask to write code)
+  
+
+REACT
+1. stateless vs stateful component
+2. what are hooks
+3. React lifecycle methods with sequece
+4. equivalent hook for each lifecycle method
+5. what is best place to make an API rquest when app loads for the first time
+6. shouldComponentUpdate in detail
+7. what is Pure Component & how it is different than normal component
+8. shallow vs deep compare
+9. context api, why it comes in picture
+10. write a counter example with state
+11. class vs functional components? which one you will prefer n why?
+12. what is Virtual DOM?
+13. which version of react did you use in your project?
+14. in your point of view, how react is better than other frameworks?
+15. how to optimize react components
+16. explain useEffect in detail
+17. state vs props
+18. setState() is synchronous or async?
+19. which pkg needs to be imported to use routing in react application
+20. what is JWT? how did you manage authentication with JWT?
+21. how did you make use of AXIOS to work with API's
+22. tell any real time scenario where we can use getSnapShotBeforeUpdate()
+23. Error Boundary usage
+24. how to achieve lazy loading in react
+
+
+
+
+REDUX
+1. what is redux? Why it is needed?
+2. context API vs REDUX,  which is better?
+3. how to connect react component with redux store
+4. explain how redux works with an example. 
+5. is it a good approach to write redux connect method in each & every compoenent? isn't it code repetition? how you will handle that case?
+6. mapStateToProps vs mapDispatchToProps 
+
+
+OTHERS
+1. what is webpack?
+2. what is babel?
+
+
+
+CODE SNIPPETS
+1. create a counter example with react (without contextApi/ redux)
+2. create a counter example with react with redux
+3. write a program to remove duplicates from an array
+4. WAP which will return second largest occured element. ['aa','bb','aa','aa','cc','cc'] o/p:- 'cc'
+5. WAP to flatten array
+6. WAP which will sort array of objects based on salary.
+7. WAP to detect palindrome
+8. WAP which will go to given API & show result on screen
+9. WAP which will show given array of object on screen in table format. also it should be deleted once click on delete btn
