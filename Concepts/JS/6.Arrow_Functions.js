b30cc984c8378b858a1f45946ce8d215c63ba95c
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-11-03 15:35:54 +0530

    Update 6.Arrow_Functions.js

--- Concepts/JS/6.Arrow_Functions.js
+++ Concepts/JS/6.Arrow_Functions.js
@@ -58,3 +58,156 @@ let sum = (a, b) => {  // the curly brace opens a multiline function
 };
 
 alert( sum(1, 2) ); // 3
+
+
+
+************************************************************************************************************************
+  
+Arrow functions over normal functions
+
+both Arrow functions & normal functions are used to define functions, but its important to decide which one to chosse.
+below are some differeneces:-
+  
+  1. this Value
+  
+  Normal Function
+    - this is dynamically scoped in normal function.i.e. value of this depends on how function got called.
+        
+    // Regular function
+        function myFunction() {
+          console.log(this);
+        }
+         myFunction(); //global obj
+    
+
+    // method Invocation
+    const myObject = {
+        method() {
+          console.log(this);
+        }
+      };
+    myObject.method(); // myObject
+
+
+    // constructor invocation
+    function MyFunction() {
+      console.log(this);
+    }
+    new MyFunction();   // MyFunction {}
+
+    // nested function
+    const myObject = {
+        myMethod() {
+            console.log(this); // logs myObject
+            function callback() {
+                console.log(this); // logs global obj
+            };
+            callback()
+        }
+    }
+
+    myObject.myMethod();
+
+
+   // to solve the above problem, we have to use below approach
+    const myObject = {
+        myMethod() {
+            var that = this;
+            console.log(this); // logs myObject
+
+            function callback() {
+                console.log(that); // logs myObject
+            };
+            callback()
+        }
+    }
+
+    myObject.myMethod();
+
+
+  // same problem can be solved using arrow function.  
+
+  Arrow Function
+    - the behaviour of this inside an arrow function is different from normal function.
+    - this is lexically scoped in Arrow function. i.e value of this depends on where it is written.
+    - it doesnt define its own execution context
+    - THIS VALUE INSIDE OF AN ARROW FUNCTION ALWAYS EQUALS THIS VALUE FROM THE OUTER FUNCTION
+    
+    const myObject = {
+        myMethod() {
+            console.log(this); // logs myObject
+            const callback = () => {
+                console.log(this); // logs myObject
+            };
+            callback()
+        }
+    }
+
+    myObject.myMethod();
+
+
+  2. Constructor
+    Normal function
+      - regular functions can easily construct objects
+      
+       function Car(color) {
+          this.color = color;
+        }
+        const redCar = new Car('red');
+        redCar instanceof Car; // => true
+        
+        
+     Arrow function
+       - as this resolves lexically in arrow functions, they cant be used as constructors.
+        
+        const Car = (color) => {
+          this.color = color;
+        };
+        const redCar = new Car('red');  // TypeError: Car is not a constructor
+
+
+  3. Arguments object
+    arrow functions dont have "arguments" object
+    
+    function myRegularFunction() {
+      const myArrowFunction = () => {
+        console.log(arguments);    // here as arrow fn do not have arguments, they print arguments from its parent function  // logs { 0: 'a', 1: 'b', length: 2 }
+      }
+      myArrowFunction('c', 'd');
+    }
+    myRegularFunction('a', 'b'); // logs { 0: 'a', 1: 'b', length: 2 }
+  
+
+
+    4. methods
+      Normal Function
+      
+      class Hero {
+          constructor(heroName) {
+              this.heroName = heroName;
+          }
+          logName() {
+              console.log(this.heroName);   // undefined.... here this will point to the Timeout obj of settimeout
+          }
+      }
+      const batman = new Hero('Batman');
+
+      setTimeout(batman.logName, 1000);
+  
+        
+      Arrow function
+
+      class Hero {
+          constructor(heroName) {
+              this.heroName = heroName;
+          }
+          logName = () => {    // here it binds this lexically to the class instance
+              console.log(this.heroName);   // Batman .. as we have arrow fn here, it doesnt matter how it got called... it checks  where it is written
+          }
+      }
+      const batman = new Hero('Batman');
+
+      setTimeout(batman.logName, 1000);
+
+
+
